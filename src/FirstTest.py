769488ff03a5a9027e80bb3231dd30c837692cb3
Don Johnson <copyleftdev@gmail.com>
2014-07-18 13:36:15 -0700

    added travis script

--- src/FirstTest.py
+++ src/FirstTest.py
@@ -1,4 +1,18 @@
+import unittest
 
+def is_odd(n):
+	return n % 2 == 1
 
-def say_hello(name)
-print('Hello {}'.format(name))
+class is_Odd_Tests(unittest.TestCase):
+
+	def testOne(self):
+		self.failUnless(is_odd(1))
+
+	def testTwo(self):
+		self.failIf(is_odd(2))
+
+def main():
+	unittest.main()
+
+if __name__ == '__main__':
+	main()
